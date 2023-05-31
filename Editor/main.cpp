7febf5539f4ad25647629f621351ccc36a738c84
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-05-31 13:37:12 +0900

    FEAT : Test Engine Folder
    Test Engine Folder have all studying and testing DirectX11 result img
    Additionally SetProcessDPIAware. That fit window magnification

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -28,6 +28,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+    //메모리릭(누수) 찾기
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    //_CrtSetBreakAlloc(402);
+    _CrtDumpMemoryLeaks();
+
+    SetProcessDPIAware();
+
     // TODO: 여기에 코드를 입력합니다.
 
     // 전역 문자열을 초기화합니다.
