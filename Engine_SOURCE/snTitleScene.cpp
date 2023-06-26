16044ab8128d7804812df35494b97478529617a6
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-06-26 10:22:56 +0900

    FEAT : Create Main Scene

--- Engine_SOURCE/snTitleScene.cpp
+++ Engine_SOURCE/snTitleScene.cpp
@@ -7,7 +7,10 @@
 #include "snCamera.h"
 #include "snCameraScript.h"
 
+#include "snSceneManager.h"
+
 #include "snResources.h"
+#include "snInput.h"
 
 namespace sn
 {
@@ -20,12 +23,12 @@ namespace sn
 	void TitleScene::Initialize()
 	{
 		{
-			//배경 이미지1 생성s
+			//배경 이미지1 생성
 			GameObject* Background = new GameObject();
 			AddGameObject(eLayerType::Background, Background);
 			MeshRenderer* mr = Background->AddComponent<MeshRenderer>();
 			mr->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
-			mr->SetMaterial(Resources::Find<Material>(L"TitleImageMaterial01"));
+			mr->SetMaterial(Resources::Find<Material>(L"TitleBackgroundMaterial01"));
 			Background->GetComponent<Transform>()->SetPosition(Vector3(0.0f, 0.0f, 0.0f));
 			//Background->GetComponent<Transform>()->SetScale(Vector3(6.7f, 4.0f, 2.0f));
 			Background->GetComponent<Transform>()->SetScale(Vector3(9.77777735f, 5.5f, 2.0f));
@@ -37,7 +40,7 @@ namespace sn
 			AddGameObject(eLayerType::Background, Background);
 			MeshRenderer* mr = Background->AddComponent<MeshRenderer>();
 			mr->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
-			mr->SetMaterial(Resources::Find<Material>(L"TitleImageMaterial02"));
+			mr->SetMaterial(Resources::Find<Material>(L"TitleBackgroundMaterial02"));
 			Background->GetComponent<Transform>()->SetPosition(Vector3(0.0f, 0.0f, 0.0f));
 			//Background->GetComponent<Transform>()->SetScale(Vector3(6.7f, 4.0f, 2.0f));
 			Background->GetComponent<Transform>()->SetScale(Vector3(9.77777735f, 5.5f, 2.0f));
@@ -49,7 +52,7 @@ namespace sn
 			AddGameObject(eLayerType::Player, camera);
 			camera->GetComponent<Transform>()->SetPosition(Vector3(0.0f, 0.0f, -10.0f));
 			Camera* cameraComp = camera->AddComponent<Camera>();
-			camera->AddComponent<CameraScript>();
+			//camera->AddComponent<CameraScript>();
 		}
 	}
 	void TitleScene::Update()
@@ -63,5 +66,17 @@ namespace sn
 	void TitleScene::Render()
 	{
 		Scene::Render();
+
+		if (Input::GetKey(eKeyCode::SPACE))
+		{
+			SceneManager::LoadScene(L"MainScene");
+		}
+	}
+	void TitleScene::OnEnter()
+	{
+		Initialize();
+	}
+	void TitleScene::OnExit()
+	{
 	}
 }
